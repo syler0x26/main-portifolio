27c61cac512a6453a9068d2565077597d0c0de87
we.sy0x26ler <salcides741@gmail.com>
2025-02-22 20:43:07 -0300

    atu

--- estilo/script.js
+++ estilo/script.js
@@ -1,27 +1,6 @@
-function mudouTamanho(){
-    if (window.innerWidth >= 568){
-        itens.style.display = 'block'
-    }else{
-        itens.style.display = 'none'
-    }
-}
-function clickMenu(){
-    if (itens.style.display == 'block'){
-        itens.style.display = 'none'
-    } else{
-        itens.style.display = 'block'
-    }
-}
-var radio = document.querySelector('.manual-btn')
-var cont = 1
-document.getElementById('radio1').checked = true
-setInterval(() => {
-    proximaImg()
-}, 5000)
-function  proximaImg(){
-    cont++
-    if(cont > 3){
-        cont = 1
-    }
-    document.getElementById('radio'+cont).checked = true
-}
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
